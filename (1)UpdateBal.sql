89ecf02eb5d3d22bf883f22bef82411b4c8644f5
DiyaWadhwani <54880903+DiyaWadhwani@users.noreply.github.com>
2019-11-25 01:18:04 +0530

    Update (1)UpdateBal.sql

--- (1)UpdateBal.sql
+++ (1)UpdateBal.sql
@@ -1,1 +1,25 @@
-/*Write*/
+set serveroutput on
+
+begin
+ 
+for rec in (select * from branch_detail)
+ loop
+ dbms.output_put.line( 'code : ' || rec.code || 'name : ' || rec.name || 'assets : ' || rec.assets);
+ end loop;
+for rec in (select * from customer_detail)
+ loop
+ dbms.output_put.line( 'ssn : ' || rec.ssn || 'name : ' || rec.name || 'place : ' || rec.place);
+ end loop;
+for rec in (select * from account_detail)
+ loop 
+ dbms.output_put.line( 'accno : ' || rec.accno || 'ssn : ' || rec.ssn || 'code : ' || rec.code || 'bal : ' || rec.bal);
+ end loop;
+
+update account_detail
+set balance = 120000
+where ssn=1;
+
+dbms.output_put.line(to_char(sql%rowcount) || ' rows affected');
+
+end;
+/
